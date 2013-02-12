5bebcef969b05065da3e4e2d8155943a2bb1124e
Lorn Potter <lorn.potter@jollamobile.com>
2013-02-12 12:19:53 +1000

    Make sure signals get sent to correct type.

--- plugin/technologymodel.h
+++ plugin/technologymodel.h
@@ -23,12 +23,12 @@
  */
 class TechnologyModel : public QAbstractListModel
 {
-    Q_OBJECT;
-    Q_DISABLE_COPY(TechnologyModel);
+    Q_OBJECT
+    Q_DISABLE_COPY(TechnologyModel)
 
-    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged);
-    Q_PROPERTY(bool available READ isAvailable NOTIFY availabilityChanged);
-    Q_PROPERTY(bool powered READ isPowered WRITE setPowered NOTIFY poweredChanged);
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(bool available READ isAvailable NOTIFY availabilityChanged)
+    Q_PROPERTY(bool powered READ isPowered WRITE setPowered NOTIFY poweredChanged)
 
 public:
     enum ItemRoles {
@@ -58,6 +58,7 @@ signals:
     void availabilityChanged(const bool &available);
     void poweredChanged(const bool &powered);
     void technologiesChanged();
+    void stateChanged(const QString &state);
 
     void scanRequestFinished();
 
@@ -71,6 +72,8 @@ private slots:
     void updateTechnologies();
     void updateServiceList();
     void managerAvailabilityChanged(bool available);
+    void changedPower(bool);
+    void finishedScan();
 };
 
 #endif // TECHNOLOGYMODEL_H

2221c126db3b0cef2ace4d4e82dddce22a7db7ac
Lorn Potter <lorn.potter@jollamobile.com>
2013-05-02 05:03:02 +1000

    make output of example better

--- examples/counters/networkcounter.cpp
+++ examples/counters/networkcounter.cpp
@@ -1,17 +1,8 @@
 /****************************************************************************
 **
 ** Copyright (C) 2013 Jolla Ltd
-** Contact: lorn.potter@gmail.com
+** Contact: lorn.potter@jollamobile.com
 **
-**
-** $QT_BEGIN_LICENSE:LGPL$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia.  For licensing terms and
-** conditions see http://qt.digia.com/licensing.  For further information
-** use the contact form at http://qt.digia.com/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
@@ -21,25 +12,13 @@
 ** ensure the GNU Lesser General Public License version 2.1 requirements
 ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
-** In addition, as a special exception, Digia gives you certain additional
-** rights.  These rights are described in the Digia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
-**
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
 
 #include "networkcounter.h"
 #include <connman-qt/counter.h>
+#include <connman-qt/networkservice.h>
 #include <QtCore>
 
 NetworkCounter::NetworkCounter(QObject *parent) :
@@ -59,7 +38,15 @@ NetworkCounter::NetworkCounter(QObject *parent) :
 void NetworkCounter::counterChanged(const QString servicePath, const QVariantMap &counters,  bool roaming)
 {
     QTextStream out(stdout);
-    out  << endl << Q_FUNC_INFO << " " << servicePath << " " << (roaming ? "roaming" : "home") << endl;
+    NetworkService service;
+    service.setPath(servicePath);
+
+    out << service.name()
+        << (roaming ? "  roaming" : "  home")
+        << "   bytes received: " << counters["RX.Bytes"].toUInt()
+        << "   bytes transmitted: " << counters["TX.Bytes"].toUInt()
+        << "   seconds: " << counters["Time"].toUInt()
+        << endl;
 }
 
 void NetworkCounter::bytesReceivedChanged(quint32 bytesRx)

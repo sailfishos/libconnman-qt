sailfishos/libconnman-qt

commit ed9d0692bee7024fa964810cd1c6cbdbe420cb80

[libconnman-qt] don't allow defaultRoute that is not online state.
This aligns more closely with the convention of 'default route',
where there can be none.

libconnman-qt/networkmanager.cpp
@@ -357,6 +357,14 @@ void NetworkManager::updateSavedServices(const ConnmanObjectList &changed)
 
 void NetworkManager::updateDefaultRoute(NetworkService* defaultRoute)
 {
+    if (defaultRoute->state() != "online") {
+        NetworkService *tempSer;
+        tempSer = new NetworkService("/",QVariantMap(),this);
+        m_defaultRoute = tempSer;
+        emit defaultRouteChanged(m_defaultRoute);
+        return;
+    }
+
     if (m_defaultRoute != defaultRoute) {
         m_defaultRoute = defaultRoute;
         emit defaultRouteChanged(m_defaultRoute);

libconnman-qt/networkservice.cpp
@@ -354,7 +354,7 @@ void NetworkService::setPath(const QString &path)
             return;
         }
 
-        if (m_propertiesCache.isEmpty()) {
+        if (m_propertiesCache.isEmpty() && path.count() > 2) {
             QDBusPendingReply<QVariantMap> reply = m_service->GetProperties();
             reply.waitForFinished();
             if (reply.isError()) {

commit a3a43121d10836879ba303d9d9aa26bd88492f99

[libconnman-qt] Fix crash when no services are available
Bug introduced by ed9d0692bee7024fa964810cd1c6cbdbe420cb80

libconnman-qt/networkmanager.cpp
@@ -357,7 +357,7 @@ void NetworkManager::updateSavedServices(const ConnmanObjectList &changed)
 
 void NetworkManager::updateDefaultRoute(NetworkService* defaultRoute)
 {
-    if (defaultRoute->state() != "online") {
+    if (defaultRoute && defaultRoute->state() != "online") {
         NetworkService *tempSer;
         tempSer = new NetworkService("/",QVariantMap(),this);
         m_defaultRoute = tempSer;